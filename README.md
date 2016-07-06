a229463882524d27305a3c5ab51f3642be0a58a1
mricher <maximilien.richer@zenika.com>
2016-07-06 12:01:39 +0200

    Add /burn

--- README.md
+++ README.md
@@ -17,3 +17,17 @@ Host: 0.0.0.0:32769
 User-Agent: curl/7.35.0
 Accept: */*
 ```
+
+Burn
+----
+
+Use `/burn` to produce a heavy CPU load. Current iteration number (`100000`) is the reference for a `i7-4800MQ` CPU.
+
+### Curl-it
+```
+while true    
+do
+  curl -H Host:whoami2.docker.localhost http://localhost/burn
+  sleep 0.05
+done
+```
--- app.go
+++ app.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/websocket"
 	// "github.com/pkg/profile"
 	"log"
+	"math"
 	"net"
 	"net/http"
 	"net/url"
@@ -15,6 +16,7 @@ import (
 )
 
 var port string
+var running int
 
 func init() {
 	flag.StringVar(&port, "port", "80", "give me a port number")
@@ -30,6 +32,7 @@ func main() {
 	flag.Parse()
 	http.HandleFunc("/echo", echoHandler)
 	http.HandleFunc("/bench", benchHandler)
+	http.HandleFunc("/burn", burnHandler)
 	http.HandleFunc("/", whoamI)
 	http.HandleFunc("/api", api)
 	fmt.Println("Starting up on port " + port)
@@ -66,6 +69,29 @@ func echoHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+}
+
+func burnHandler(w http.ResponseWriter, req *http.Request) {
+	go pi(100000) // TODO iterations as GET parameter
+	whoamI(w, req)
+}
+
+// Pi launches n goroutines to compute an
+// approximation of pi.
+func pi(n int) float64 {
+	ch := make(chan float64)
+	for k := 0; k <= n; k++ {
+		go term(ch, float64(k))
+	}
+	f := 0.0
+	for k := 0; k <= n; k++ {
+		f += <-ch
+	}
+	return f
+}
+
+func term(ch chan float64, k float64) {
+	ch <- 4 * math.Pow(-1, k) / (2*k + 1)
 }
 
 func whoamI(w http.ResponseWriter, req *http.Request) {
